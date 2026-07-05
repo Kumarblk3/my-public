58a1946ffe2c7dd52975017f8725d5ad99b81283
K B L KUMAR <kumaraimbig@gmail.com>
2026-07-06 01:39:39 +0530

    to-update-vpc-subnets (#11)
    * to-update-vpc-subnets
    
    * to-update-vpc-subnets
    
    * to-update-vpc-subnets

--- ec2.tf
+++ ec2.tf
@@ -2,18 +2,25 @@ provider "aws" {
   region = var.aws_region
 }
 
+resource "aws_vpc" "my-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "my-subnet" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-2a"
+}
+
 resource "aws_instance" "practice_ec2" {
   ami           = "ami-0ffa797f35095b9f7"   # Amazon Linux 2 AMI for ap-south-1
   instance_type = "t3.micro"
   key_name = var.aws_keypair
-
+  subnet_id     = aws_subnet.my-subnet.id
 
   tags = {
     Name = "PracticeEC2"
   }
-}
-resource "aws_s3_bucket" "my-bucket" {
-  bucket = "my-fist-s3-bucket-21"
 }
 
 variable "aws_region" {
